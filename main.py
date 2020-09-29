9593430f8992c02a3559a71838a6bd318aaa00ab
shellkore <shellkoresahu@gmail.com>
2020-09-30 04:27:50 +0530

    timezone apis made

--- main.py
+++ main.py
@@ -17,6 +17,10 @@ def index():
 def get_cities():
 	return db
 
+@app.get('/cities/{city_id}')
+def get_city(city_id: int):
+	return db[city_id-1]
+
 @app.post('/cities')
 def create_city(city: City): #type hints. City class is used to tell api the expected type of data
 	db.append(city.dict())
@@ -24,5 +28,6 @@ def create_city(city: City): #type hints. City class is used to tell api the exp
 
 @app.delete('/cities/{city_id}')
 def delete_city(city_id:int): #automatic validation because of type hint set to 'int'
+	deleted = db[city_id-1]
 	db.pop(city_id-1)
-	return {'msg':'deleted'}
+	return {'delted': deleted}
