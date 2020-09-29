8c83bc0a8dc9a0b939f860f2fc09a4461c8bd2d6
shellkore <shellkoresahu@gmail.com>
2020-09-30 04:12:32 +0530

    get, post and delete api

--- main.py
+++ main.py
@@ -1,7 +1,28 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+
+class City(BaseModel):
+	name: str
+	timezone: str
+
+db= []
 
 @app.get('/') # instead of app.route
 def index():
 	return {'key':'value'}
+
+@app.get('/cities')
+def get_cities():
+	return db
+
+@app.post('/cities')
+def create_city(city: City): #type hints. City class is used to tell api the expected type of data
+	db.append(city.dict())
+	return db[-1]
+
+@app.delete('/cities/{city_id}')
+def delete_city(city_id:int): #automatic validation because of type hint set to 'int'
+	db.pop(city_id-1)
+	return {'msg':'deleted'}
