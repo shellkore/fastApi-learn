dfafa69f134d8ac4dc337a0ddd3031d20fd7731e
shellkore <shellkoresahu@gmail.com>
2021-03-23 22:08:51 +0530

    heroku runtime to 3.7.10

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 from typing import Optional
 import uvicorn
+import base64
 
 from ImageSteganography import hideData, showData
 
@@ -11,7 +12,42 @@ class City(BaseModel):
 	name: str
 	timezone: str
 
+class ImageInput(BaseModel):
+    msg: str
+    image_string: str
+    pass_key: Optional[str] = None
+
 db= []
+
+def encrypt(msg,password):
+    msgList = list(msg)
+    pswdList = list(password)
+    msglen = len(msgList)
+    pswdlen = len(pswdList)
+
+    pswdPos = 0
+    for i in range(msglen):
+        if(pswdPos==len(pswdList)):
+            pswdPos=0
+        msgList[i]= chr(ord(msgList[i])+ord(pswdList[pswdPos]))
+        pswdPos+=1
+
+    return (''.join(msgList))
+
+def decrypt(msg,password):
+    msgList = list(msg)
+    pswdList = list(password)
+    msglen = len(msgList)
+    pswdlen = len(pswdList)
+
+    pswdPos = 0
+    for i in range(msglen):
+        if(pswdPos==len(pswdList)):
+            pswdPos=0
+        msgList[i]= chr(ord(msgList[i])-ord(pswdList[pswdPos]))
+        pswdPos+=1
+
+    return (''.join(msgList))
 
 @app.get('/') # instead of app.route
 def index():
@@ -41,16 +77,22 @@ async def get_item(item_id: int, q: Optional[str] = None):
 	return {'item_id':item_id,'q':q}
 
 @app.post('/imgencode')
-async def img_encode(msg: str, key: Optional[str] = None):
-	imageName = 'image/input.png'
-	res = hideData(imageName,msg)
-	return res
+async def img_encode(data:ImageInput):
+    imageName = 'image/input1.png'
+    imgdata = base64.b64decode(data.image_string)
+    print(data.msg)
+    # print(type(pass_key))
+    print(data.pass_key)
+    with open (imageName,'wb') as imgFile:
+        imgFile.write(imgdata)
+    return "done"
 
 @app.post('/imgdecode')
 async def img_decode(key: Optional[str] = None):
 	imageName = 'image/output.png'
 	res = showData(imageName)
-	return res
+	newRes = decrypt(res,key)
+	return newRes
 
 if __name__ == '__main__':
 	uvicorn.run(app)
@@ -58,3 +100,4 @@ if __name__ == '__main__':
 # gunicorn
 # uvloop
 # httptools
+# numpy
