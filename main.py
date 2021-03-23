e3b428e9b1f356f8b3743c04a533cac2d7fae542
shellkore <shellkoresahu@gmail.com>
2021-03-24 03:01:25 +0530

    implemented websocket for chat

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect,Request
 from pydantic import BaseModel
 from typing import Optional
 import uvicorn
 import base64
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import HTMLResponse
 
 from ImageSteganography import hideData, showData
 
 app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+templates = Jinja2Templates(directory="templates")
 
 class City(BaseModel):
 	name: str
@@ -93,6 +100,45 @@ async def img_decode(key: Optional[str] = None):
 	res = showData(imageName)
 	newRes = decrypt(res,key)
 	return newRes
+
+class ConnectionManager:
+    def __init__(self):
+        self.active_connections: List[WebSocket] = []
+
+    async def connect(self, websocket: WebSocket):
+        await websocket.accept()
+        self.active_connections.append(websocket)
+
+    def disconnect(self, websocket: WebSocket):
+        self.active_connections.remove(websocket)
+
+    async def send_personal_message(self, message: str, websocket: WebSocket):
+        await websocket.send_text(message)
+
+    async def broadcast(self, message: str):
+        for connection in self.active_connections:
+            await connection.send_text(message)
+
+
+manager = ConnectionManager()
+
+
+@app.get("/chat",response_class=HTMLResponse)
+async def chat_websocket(request: Request):
+    return templates.TemplateResponse("chat.html",{"request": request})
+
+
+@app.websocket("/ws/{client_id}")
+async def websocket_endpoint(websocket: WebSocket, client_id: str):
+    await manager.connect(websocket)
+    try:
+        while True:
+            data = await websocket.receive_text()
+            await manager.send_personal_message(f"You wrote: {data}", websocket)
+            await manager.broadcast(f"Client #{client_id} says: {data}")
+    except WebSocketDisconnect:
+        manager.disconnect(websocket)
+        await manager.broadcast(f"Client #{client_id} left the chat")
 
 if __name__ == '__main__':
 	uvicorn.run(app)
